3dedecaa0d9c99efc54b1e7bb050ac7541cb07f2
selimbaba67 <selo_melo67@hotmail.com>
2019-12-16 21:43:12 +0100

    first commit

--- push.sh
+++ push.sh
@@ -1,2 +1,4 @@
-git -C /Users/akca/Documents/GitHub/mym3u/ add readme.m3u
+git -C /Users/akca/Documents/GitHub/mym3u/ add README.m3u
+git -C /Users/akca/Documents/GitHub/mym3u/ add push.sh
+git -C /Users/akca/Documents/GitHub/mym3u/ commit -m "first commit"
 git -C /Users/akca/Documents/GitHub/mym3u/ push -u origin master
